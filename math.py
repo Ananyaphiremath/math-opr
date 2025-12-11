4218c399a54aa500c2c6eecc33a3437b5c2ee8a8
Ananyaphiremath <hiremathananya402@gmail.com>
2025-12-11 19:55:21 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,8 @@
+4. wring code in remote     
+try: 
+    a = float(input("Enter first number: ")) 
+    b = float(input("Enter second number: ")) 
+    print(f"Addition: {a + b}") 
+    print(f"Subtraction: {a - b}") 
+except ValueError: 
+    print("Please enter valid numbers.") 
